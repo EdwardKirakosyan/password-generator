6365a9ec7e3b0771c86b5ac24dc27b4c216ef8d0
EdwardKir <kirakosyan3010@gmail.com>
2023-05-05 21:14:49 +0300

    add

---
+++ index.js
@@ -0,0 +1,117 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+]
+
+const genBtn = document.getElementById("button-gen")
+const firstInput = document.getElementById("first-input")
+const secondInput = document.getElementById("second-input")
+
+let randomCharLength = 15
+
+function makeRandChar() {
+  let randChar = Math.floor(Math.random() * characters.length)
+  return characters[randChar]
+}
+
+function makeRandPas() {
+  let randPas = ""
+  for (let i = 0; i < randomCharLength; i++) {
+    randPas += makeRandChar()
+  }
+  return randPas
+}
+
+genBtn.addEventListener("click", function () {
+  firstInput.value = makeRandPas()
+  secondInput.value = makeRandPas()
+})
